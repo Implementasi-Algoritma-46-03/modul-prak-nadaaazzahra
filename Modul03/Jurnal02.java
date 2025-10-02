40e11090a0f8ab5ecd89af71f2bb53bd994ddaaf
nadaaazzahra <nadaazahra150906@gmail.com>
2025-10-02 18:11:35 +0700

    Mengumpulkan Junal03

--- Modul03/Jurnal02.java
+++ Modul03/Jurnal02.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Jurnal02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        //input
+        double nilai1 = input.nextDouble();
+        double nilai2 = input.nextDouble();
+        double nilai3 = input.nextDouble();
+
+        double nilaiRataRata = (nilai1 + nilai2 + nilai3) / 3.0;
+
+        System.out.printf("Nilai rata-rata : %.2f", nilaiRataRata);
+
+
     }
 }
