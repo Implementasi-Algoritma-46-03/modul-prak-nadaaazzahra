c88128edcb967ba787cfcfb779caa79dbca63e3b
nadaaazzahra <nadaazahra150906@gmail.com>
2025-10-05 23:10:53 +0700

    mengumpulkan TP 1,2,3

--- Modul04/TP01.java
+++ Modul04/TP01.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class TP01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        //input nilai N
+        int n = input.nextInt();
+
+        //cek nilai N apakah kelipatan
+        if (n % 6 == 0) {
+            System.out.println("Kelipatan Enam");
+        } else {
+            System.out.println("Bukan Kelipatan Enam");
+        }
+
+
     }
 }
--- Modul04/TP02.java
+++ Modul04/TP02.java
@@ -1,6 +1,28 @@
-public class TP02 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class TP02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        double total = N;
+        double diskon = 0;
+
+        if (N > 200000) {
+            diskon = N * 0.05;
+            total = N - diskon;
+            System.out.println("Transaksi = " + N);
+            System.out.println("Total = " + N + " - " + (int)diskon);
+        } else {
+            System.out.println("Transaksi = " + N);
+            System.out.println("Total = " + N);
+        }
+
+        double ppn = total * 0.11;
+        double totalDibayar = total + ppn;
+
+        System.out.println("Ppn 11% = " + ppn);
+        System.out.println("Total dibayar = " + totalDibayar);
+
+        input.close();
     }
 }
