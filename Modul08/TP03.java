982ab9653e6619c5b074f5d74d8f1628b95dd5da
nadaaazzahra <nadaazahra150906@gmail.com>
2025-11-06 17:24:11 +0700

    modul08

--- Modul08/TP03.java
+++ Modul08/TP03.java
@@ -1,6 +1,19 @@
-public class TP03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class TP03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int total = 0;q   // menyimpan total risoles
+        int jumlah;      // untuk menyimpan input sementara
+
+        jumlah = input.nextInt(); // baca angka pertama
+
+        while (jumlah != 0) {    // berhenti jika input 0
+            total += jumlah;     // tambahkan ke total
+            jumlah = input.nextInt(); // baca angka berikutnya
+        }
+
+        System.out.println(total); // tampilkan total akhir
     }
 }
