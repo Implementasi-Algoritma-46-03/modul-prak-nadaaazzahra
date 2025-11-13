07728b285e9ff1f21921e7ca127c13a0b5907060
nadaaazzahra <nadaazahra150906@gmail.com>
2025-11-13 16:46:33 +0700

    up modul09

--- Modul09/Jurnal01.java
+++ Modul09/Jurnal01.java
@@ -1,6 +1,9 @@
 public class Jurnal01 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        for (int i = 1; i <= 10; i++){
+            int kuadrat = i * i;
+            System.out.print(kuadrat + " ");
+        }
     }
 }
